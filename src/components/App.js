c95c982f602d3d9ce278ab6f308c06490975515a
shams220 <103259015+shams220@users.noreply.github.com>
2025-12-11 09:43:02 +0530

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,24 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
+import Todos from "./todos";
 
 const App = () => {
+
+  const [todos,setTodos] = useState([ 
+    {id:1,text:"learn React",isComplete:false},
+    {id:2,text:"Build a React App",isComplete:false},
+    {id:3,text:"Deploy the React app",isComplete:false}
+  ]);
+ 
+  console.log(todos)
+
   return (
     <div>
+      <h1>Parent Component</h1>
         {/* Do not remove the main div */}
+   
+<Todos tasks={todos} setTodos= {setTodos} />
     </div>
   )
 }
